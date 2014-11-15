620c3cae0f6e5cf91e7190e7c8a06976d2b60e0e
JasonTsao <lanevo6tmegsr@gmail.com>
2014-11-15 12:59:04 -0800

    adding some stuff for root urls

--- Fashion/urls.py
+++ Fashion/urls.py
@@ -1,11 +1,14 @@
+import settings
 from django.conf.urls import patterns, include, url
-
+from django.contrib import admin
+admin.autodiscover()
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^admin/', include(admin.site.urls)),
     # url(r'^$', 'Fashion.views.home', name='home'),
     # url(r'^Fashion/', include('Fashion.foo.urls')),
 
@@ -14,4 +17,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    #IMPORTANT FOR SERVING FILES ON NGINX ON LIVE DEPLOY
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', 
+    {'document_root': settings.STATIC_ROOT, 'show_indexes':True}), 
 )
